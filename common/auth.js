479b1e9850904050ddae88a647c92d9b6f7b1c95
Leo <leoskywork@outlook.com>
2020-10-21 13:33:32 +0800

    add comments

--- common/auth.js
+++ common/auth.js
@@ -3,70 +3,73 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 class Auth {
-	configPassport(passport) {
-		if (!passport) throw 'passport required';
+    configPassport(passport) {
+        if (!passport) throw 'passport required';
 
-		passport.use(
-			new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-				if (!email) throw 'email required';
+        passport.use(
+            new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+                if (!email) throw 'email required';
 
-				User.findOne({ emailLower: String(email).toLowerCase() }, (err, user) => {
-					if (err) this.handleError(err, 'User.findOne', true);
+                User.findOne({ emailLower: String(email).toLowerCase() }, (err, user) => {
+                    if (err) this.handleError(err, 'User.findOne', true);
 
-					if (!user) return done(null, false, { message: `Email ${email} is not registered` });
+                    //params for done method: (error, user, verifyOptions)
+                    if (!user) return done(null, null, { message: `Email ${email} is not registered` });
 
-					bcrypt.compare(password, user.password, (err, match) => {
-						if (err) this.handleError(err, 'bcrypt.compare', true);
+                    bcrypt.compare(password, user.password, (pwdErr, match) => {
+                        if (pwdErr) this.handleError(pwdErr, 'bcrypt.compare', true);
 
-						this.log('bcrypt.compare result', match);
+                        this.log('auth bcrypt.compare result', match);
 
-						if (match) {
-							return done(null, user);
-						} else {
-							return done(null, false, { message: 'password incorrect' });
-						}
-					});
-				});
-			})
-		);
+                        if (match) {
+                            return done(null, user);
+                        } else {
+                            return done(null, null, { message: 'password incorrect' });
+                        }
+                    });
+                });
+            })
+        );
 
-		passport.serializeUser((user, done) => {
-			this.log('serialize user ' + user.emailLower, user.id);
+        passport.serializeUser((user, done) => {
+            this.log('serialize user ' + user.emailLower, user.id);
 
-			//note: this function will be called if req.login() passes, only the user.id is serialized to the session,
-			//  keeping the amount of data stored within the session small, when subsequent requests are received, this id
-			//  is used to find the user(ref deserializeUser()), which will be restored to req.user.
-			done(null, user.id);
-		});
+            //note: this function will be called if req.login() passes, only the user.id is serialized to the session,
+            //  keeping the amount of data stored within the session small, when subsequent requests are received, this id
+            //  is used to find the user(ref deserializeUser()), which will be restored to req.user.
+            done(null, user.id);
+        });
 
-		passport.deserializeUser((id, done) => {
-			this.log('deserialize user', id);
+        passport.deserializeUser((id, done) => {
+            this.log('deserialize user', id);
 
-			User.findById(id, (err, user) => {
-				if (err) {
-					this.handleError(err, 'deserialize user');
-				}
-				done(err, user);
-			});
-		});
-	}
+            User.findById(id, (err, user) => {
+                if (err) {
+                    this.handleError(err, 'deserialize user');
+                    return done(err);
+                }
 
-	//?? seems not working
-	// desensitizeUser(user) {
-	// 	delete user.password;
-	// 	delete user.emailLower;
-	// 	return user;
-	// }
+                done(null, user);
+            });
+        });
+    }
 
-	//todo: error handling
-	handleError(error, source = 'auth', rethrow = false) {
-		console.log(source + ':', error);
-		if (rethrow) throw error;
-	}
+    //?? seems not working
+    // desensitizeUser(user) {
+    // 	delete user.password;
+    // 	delete user.emailLower;
+    // 	return user;
+    // }
 
-	log(source, message) {
-		console.log(source + ':', message);
-	}
+    //todo: error handling
+    handleError(error, source = 'auth', rethrow = false) {
+        console.log(source + ':', error);
+        if (rethrow) throw error;
+    }
+
+    log(source, message) {
+        console.log(source + ':', message);
+    }
 }
 
 module.exports = Auth;
--- routes/users.js
+++ routes/users.js
@@ -21,153 +21,157 @@ router.get('/login', (req, resp) => resp.send({ api: 'login.get' }));
 //and any additional route handlers will not be invoked.
 //If authentication succeeds, the next handler will be invoked and the req.user property will be set to the authenticated user.
 router.post('/login', (req, resp, next) => {
-	/*//can't redirect here since using angular as a separate frontend
-	passport.authenticate('local', {
-			successRedirect: '/feed',
-			failureRedirect: '/faq',
-			failureFlash: true
-	})(req, resp, next);
+    console.log('----- enter /login');
+    /*//can't redirect here since using angular as a separate frontend
+    passport.authenticate('local', {
+            successRedirect: '/feed',
+            failureRedirect: '/faq',
+            failureFlash: true
+    })(req, resp, next);
     return; */
 
-	//note: authenticate and return, use custom callback(instead of built in options - see above)
-	//  - need manually call req.login() to establish a session in this case
-	//[ref](https://www.djamware.com/post/5a878b3c80aca7059c142979/securing-mean-stack-angular-5-web-application-using-passport)
-	passport.authenticate('local', (err, user, verifyOptions) => {
-		console.log('passport auth callback:', err, user ? user.email : null, verifyOptions);
-		const verifyMessage = verifyOptions != null ? verifyOptions.message : null;
+    //note: authenticate and return, use custom callback(instead of built in options - see above)
+    //  - need manually call req.login() to establish a session in this case
+    //[ref](https://www.djamware.com/post/5a878b3c80aca7059c142979/securing-mean-stack-angular-5-web-application-using-passport)
+    passport.authenticate('local', (authErr, user, verifyOptions) => {
+        console.log('passport auth callback:', authErr, user ? user.email : null, verifyOptions);
+        const verifyMessage = verifyOptions != null ? verifyOptions.message : null;
 
-		if (err) {
-			handleError('login authenticate', err);
-			resp.json(new ApiResult(false, null, verifyMessage));
-			return;
-		}
+        if (authErr) {
+            handleError('login authenticate', authErr);
+            resp.json(new ApiResult(false, null, verifyMessage));
+            return;
+        }
 
-		if (!user) {
-			resp.status(401).send(new ApiResult(false, null, verifyMessage));
-		}
+        if (!user) {
+            resp.status(401).send(new ApiResult(false, null, verifyMessage));
+        }
 
-		req.login(user, err => {
-			if (err) {
-				handleError(err, 'req.login');
-				next(err);
-				return;
-			}
+        //create a login session after a successful auth
+        req.login(user, err => {
+            if (err) {
+                handleError(err, 'req.login');
+                next(err);
+                return;
+            }
 
-			//resp.redirect('/feed'); return; //do the redirect on angular side
-			resp.json(new ApiResult(true, desensitizeUser(user)));
-		});
-	})(req, resp, next);
+            //resp.redirect('/feed'); return; //do the redirect on angular side
+            resp.json(new ApiResult(true, desensitizeUser(user)));
+        });
+    })(req, resp, next);
 });
 
 router.get('/logout', (req, resp) => {
-	req.logout();
-	resp.json(new ApiResult(true, 'You have logged out'));
+    console.log('----- enter /logout');
+    req.logout();
+    resp.json(new ApiResult(true, 'You have logged out'));
 });
 
 router.get('/register', (req, resp) => {
-	//res.send('register.get ');
-	resp.send('{"api": "register.get"}');
+    //res.send('register.get ');
+    resp.send('{"api": "register.get"}');
 });
 
 router.post('/register', (req, resp) => {
-	console.log('post register:', req.body);
-	//resp.send('{"api": "register.post"}'); return;
-	const { email, password, password2 } = req.body;
-	const errors = validateRegisterInfo(email, password, password2);
+    console.log('post register:', req.body);
+    //resp.send('{"api": "register.post"}'); return;
+    const { email, password, password2 } = req.body;
+    const errors = validateRegisterInfo(email, password, password2);
 
-	if (errors.length > 0) {
-		resp.send(new ApiResult(false, { regInfo: desensitizeReq(req.body) }, errors));
-		return;
-	}
+    if (errors.length > 0) {
+        resp.send(new ApiResult(false, { regInfo: desensitizeReq(req.body) }, errors));
+        return;
+    }
 
-	let { name } = req.body;
-	//assign default name if not set
-	if (!name || String(name).trim().length == 0) {
-		name = String(email).split('@')[0];
-	}
+    let { name } = req.body;
+    //assign default name if not set
+    if (!name || String(name).trim().length == 0) {
+        name = String(email).split('@')[0];
+    }
 
-	const emailLower = String(email).toLowerCase();
+    const emailLower = String(email).toLowerCase();
 
-	User.findOne({ emailLower: emailLower }).then(user => {
-		if (user) {
-			errors.push(`email already registered`);
-			resp.send(new ApiResult(false, { regInfo: desensitizeReq(req.body) }, errors));
-			return;
-		}
+    User.findOne({ emailLower: emailLower }).then(user => {
+        if (user) {
+            errors.push(`email already registered`);
+            resp.send(new ApiResult(false, { regInfo: desensitizeReq(req.body) }, errors));
+            return;
+        }
 
-		const newUser = new User({ name, email, emailLower, password });
+        const newUser = new User({ name, email, emailLower, password });
 
-		bcrypt.genSalt(AppConst.bcryptSaltRounds, (err, salt) => {
-			if (err) {
-				handleError('gen salt error', err, true);
-			}
+        bcrypt.genSalt(AppConst.bcryptSaltRounds, (err, salt) => {
+            if (err) {
+                handleError('gen salt error', err, true);
+                return;
+            }
 
-			bcrypt
-				.hash(newUser.password, salt)
-				.then(hashedPassword => {
-					newUser.password = hashedPassword;
-					newUser
-						.save()
-						.then(savedUser => {
-							//todo: impl flash message
-							//req.flash('success_msg', 'You are now registered');
-							// resp.redirect('/users/login');
-							resp.send(new ApiResult(true, desensitizeUser(savedUser), 'You are now registered'));
-						})
-						.catch(err => {
-							handleError('save-newUser-error', err, true);
-						});
-				})
-				.catch(hashErr => {
-					handleError('hash pwd error', hashErr, true);
-				});
-		});
-	});
+            bcrypt
+                .hash(newUser.password, salt)
+                .then(hashedPassword => {
+                    newUser.password = hashedPassword;
+                    newUser
+                        .save()
+                        .then(savedUser => {
+                            //todo: impl flash message
+                            //req.flash('success_msg', 'You are now registered');
+                            // resp.redirect('/users/login');
+                            resp.send(new ApiResult(true, desensitizeUser(savedUser), 'You are now registered'));
+                        })
+                        .catch(err => {
+                            handleError('save-newUser-error', err, true);
+                        });
+                })
+                .catch(hashErr => {
+                    handleError('hash pwd error', hashErr, true);
+                });
+        });
+    });
 
-	function desensitizeReq(reqBody) {
-		delete reqBody.password;
-		delete reqBody.password2;
-		return reqBody;
-	}
+    function desensitizeReq(reqBody) {
+        delete reqBody.password;
+        delete reqBody.password2;
+        return reqBody;
+    }
 
-	function validateRegisterInfo(email, password, password2) {
-		const errors = [];
+    function validateRegisterInfo(email, password, password2) {
+        const errors = [];
 
-		if (!AppUtility.validateEmail(email)) {
-			errors.push('Please input a valid email');
-		}
+        if (!AppUtility.validateEmail(email)) {
+            errors.push('Please input a valid email');
+        }
 
-		if (!password) {
-			errors.push('Please input password');
-		} else {
-			if (password != password2) {
-				errors.push('Passwords do not match');
-			}
+        if (!password) {
+            errors.push('Please input password');
+        } else {
+            if (password != password2) {
+                errors.push('Passwords do not match');
+            }
 
-			if (password.length < 6) {
-				errors.push('Password must be at least 6 characters');
-			}
-		}
+            if (password.length < 6) {
+                errors.push('Password must be at least 6 characters');
+            }
+        }
 
-		return errors;
-	}
+        return errors;
+    }
 });
 
 //todo: error handling
 function handleError(source, error, rethrow = false) {
-	console.log(source + ':', error);
+    console.log(source + ':', error);
 
-	if (rethrow) {
-		throw error;
-	}
+    if (rethrow) {
+        throw error;
+    }
 }
 
 function desensitizeUser(user) {
-	return {
-		name: user.name,
-		email: user.email,
-		registerDate: user.date
-	};
+    return {
+        name: user.name,
+        email: user.email,
+        registerDate: user.date
+    };
 }
 
 module.exports = router;
